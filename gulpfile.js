71dbc98e89f1b14561755c129d298bf13585a420
Mallory Porche <malloryburke@gmail.com>
2016-01-31 15:56:07 -0800

    Setup wiredep

--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,12 @@ var jscs = require('gulp-jscs');
 
 var jsFiles = ['*.js', 'src/**/*.js'];
 
-
-
 // Create a task to check styles//
 gulp.task('style', function (){
 	return gulp.src(jsFiles)
 		.pipe(jshint())
 		.pipe(jshint.reporter('jshint-stylish', {
 			verbose: true
-		}));
-
+		}))
+		.pipe(jscs());
 });
