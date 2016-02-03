21ab9b65f4780d7885f21d2a6acf88a2f291c379
Mallory Porche <malloryburke@gmail.com>
2016-02-02 20:47:43 -0800

    Installed MongoDB

--- app.js
+++ app.js
@@ -23,62 +23,6 @@ app.use(express.static('public'));
 app.set('views', './src/views');
 
 app.set('view engine', 'ejs');
-var router = function(nav) {
-
-var books =[
-	{
-		title: 'War and Peace',
-		genre: 'Historical Fiction',
-		author: 'Some Author',
-		read: false
-	},
-	{
-		title: 'Childhood',
-		genre: 'Biography',
-		author: 'Lev Nikolayevich Tolstoy',
-		read: false
-	},
-	{
-		title: 'Life On The Mississippi',
-		genre: 'History',
-		author: 'Mark Twain',
-		read: false
-	},
-	{
-		title: 'Les Miserables',
-		genre: 'Historical Fiction',
-		author: 'Victor Hugo',
-		read: false
-	},
-	{
-		title: 'The Time Machine',
-		genre: 'Science Fiction',
-		author: 'H.G. Wells',
-		read: false
-	}
-];
-
-	bookRouter.route('/')
-		.get(function(req, res){
-			res.render('bookListView', {
-				title: 'Books',
-				nav: nav,
-				books: books
-			});
-		});
-
-	bookRouter.route('/:id')
-		.get(function(req, res){
-			var id = req.params.id;
-			res.render('bookView', {
-				title: 'Books',
-				nav: nav,
-				book: books[id]
-			});
-	});
-
-	return bookRouter;
-}
 
 app.use('/Books', bookRouter);
 
@@ -86,7 +30,7 @@ app.use('/Books', bookRouter);
 //Express taking a request from the browser at the root and sending something back
 app.get('/', function(req, res){
 	res.render('index', {
-		title: 'Storystrap',
+		title: 'MyLibrary',
 		nav: nav
 	});
 });
@@ -97,6 +41,11 @@ app.get('/books', function(req, res){
 	});
 });
 
+app.get('/authors', function(req, res) {
+	res.render('authorView', {
+		title: 'Authors'
+	})
+})
 //Creating instance of the express app and listen on a specific port
 app.listen(port, function(err){
 	console.log('running server on port '+ port);
