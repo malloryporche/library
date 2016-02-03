d79ec2d325b2f52fcbf34844896cb3c3c1db507c
Mallory Porche <malloryburke@gmail.com>
2016-02-02 18:38:20 -0800

    Navigation error fixed

--- app.js
+++ app.js
@@ -6,7 +6,15 @@ var app = express();
 
 var port = process.env.PORT || 5000;
 
-var bookRouter = require('./src/routes/bookRoutes');;
+var nav = [{
+	Link:'/Books', 
+	Text: 'Books'
+	}, {
+	Link: '/Authors', 
+	Text: 'Authors'
+	}];
+
+var bookRouter = require('./src/routes/bookRoutes')(nav);
 
 //Setup public directory as a static directory,
 //and the view directory as a static directory to setup Middleware
@@ -15,23 +23,78 @@ app.use(express.static('public'));
 app.set('views', './src/views');
 
 app.set('view engine', 'ejs');
+var router = function(nav) {
 
-app.use('/Books', bookRouter)
+var books =[
+	{
+		title: 'War and Peace',
+		genre: 'Historical Fiction',
+		author: 'Some Author',
+		read: false
+	},
+	{
+		title: 'Childhood',
+		genre: 'Biography',
+		author: 'Lev Nikolayevich Tolstoy',
+		read: false
+	},
+	{
+		title: 'Life On The Mississippi',
+		genre: 'History',
+		author: 'Mark Twain',
+		read: false
+	},
+	{
+		title: 'Les Miserables',
+		genre: 'Historical Fiction',
+		author: 'Victor Hugo',
+		read: false
+	},
+	{
+		title: 'The Time Machine',
+		genre: 'Science Fiction',
+		author: 'H.G. Wells',
+		read: false
+	}
+];
+
+	bookRouter.route('/')
+		.get(function(req, res){
+			res.render('bookListView', {
+				title: 'Books',
+				nav: nav,
+				books: books
+			});
+		});
+
+	bookRouter.route('/:id')
+		.get(function(req, res){
+			var id = req.params.id;
+			res.render('bookView', {
+				title: 'Books',
+				nav: nav,
+				book: books[id]
+			});
+	});
+
+	return bookRouter;
+}
+
+app.use('/Books', bookRouter);
 
 
 //Express taking a request from the browser at the root and sending something back
 app.get('/', function(req, res){
 	res.render('index', {
 		title: 'Storystrap',
-		nav: [{
-			Link:'/Books', Text: 'Books'
-		}, {
-			Link: '/Authors', Text: 'Authors'
-		}]});
+		nav: nav
+	});
 });
 
 app.get('/books', function(req, res){
-	res.send('Hello books!');
+	res.render('booklistView', {
+		title: 'Book Lists'
+	});
 });
 
 //Creating instance of the express app and listen on a specific port
